efb3a06eba81a0ae05edd2b1c2be01fd8e3e7e09
Fatih Celik <30297735+fhcelik@users.noreply.github.com>
2023-11-30 08:38:44 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
 #created the main python file.
 import streamlit
+import pandas
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
+
+my_fruit_list = pandas.read.csv("https://uni-lab-files/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
